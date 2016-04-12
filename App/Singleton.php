548bf3cde23a8ab43ca8f6637e2889e647b0c2b2
Yavdat <yavdat92@mail.ru>
2016-04-12 08:31:31 +0300

    Singleton

---
+++ App/Singleton.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: serb
+ * Date: 4/12/16
+ * Time: 8:05 AM
+ */
+
+namespace App;
+
+
+abstract class Singleton
+{
+
+    protected static $instance;
+
+    protected  function __construct()
+    {
+    }
+
+    public static function instance()
+    {
+        if(null===static::$instance){
+            static::$instance=new static;
+        }
+        return new static::$instance;
+    }
+
+
+}
