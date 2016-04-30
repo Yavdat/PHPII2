36b0e9bb28df2cd02f0d31ede1a7b51a1c050e21
Yavdat92 <yavdat92@mail.ru>
2016-05-01 00:39:20 +0300

    +Active Record

--- App/Model.php
+++ App/Model.php
@@ -6,6 +6,8 @@ abstract class Model
 {
     const TABLE='';
 
+    public $id;
+
     public static function findAll()
     {
         $db=Db::instance();
@@ -14,4 +16,37 @@ abstract class Model
             static::class
         );
     }
+
+    public  function isNew() //Новая ли наша модель
+    {
+        return empty($this->id);
+    }
+
+    public function insert()
+    {
+        if(!$this->isNew()){
+            return;
+        }
+
+        $columns=[];
+        $values=[];
+        foreach ($this as $k=>$v){
+            if('id'==$k){
+                continue;
+            }
+            $columns[]=$k;
+            $values[':'.$k]=$v;
+        }
+        var_dump($values);
+
+
+        $sql='INSERT INTO ' .static::TABLE.' 
+        ('.implode(',',$columns).')
+        VALUES
+        ('.implode(',',array_keys($values)).')
+        ';
+        $db=DB::instance();
+        $db->execute($sql,$values);
+    }
+
 }
--- index.php
+++ index.php
@@ -4,18 +4,10 @@
 //{
 //    require __DIR__."/".str_replace('\\','/',$class).'.php';
 //}
-use App\Models\HasEmail;
 use App\Models\User;
 require __DIR__.'/autoload.php';
 
-$users=User::findAll();
-
-//$res=$db->execute('CREATE TABLE foo (id SERIAL)');
-function sendEmail(HasEmail $user, string $message)
-{
-    echo 'The message is sent to '.$user->email;
-}
-
-sendEmail($users[0],'Hello');
-
-//echo '<pre>'.print_r($users,1).'</pre>';
+$user=new User();
+$user->name='Vasya';
+$user->email='v@pupkin.ru';
+$user->insert();
